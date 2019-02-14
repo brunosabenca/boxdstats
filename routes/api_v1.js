891e4bcdb058746da84f4a6fe977a537eb34efe5
Bruno Sabença <bruno@brunosabenca.com>
2019-02-14 16:43:00 +0000

    Adds caching to the fetching of the user id

--- routes/api_v1.js
+++ routes/api_v1.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var cache = require('express-redis-cache')();
 
 var user_controller = require('../controllers/userController');
 var activity_controller = require('../controllers/activityController');
 
 router.get('/user/:userId', user_controller.user_get);
 router.get('/user/by-username/:userName', user_controller.user_by_username_get);
-router.get('/user/by-username/:userName/id', user_controller.id_by_username_get);
+router.get('/user/by-username/:userName/id', cache.route(), user_controller.id_by_username_get);
 
 router.get('/user/:userId/activity', activity_controller.activity_get);
 
